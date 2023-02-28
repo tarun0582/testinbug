407cf8af7f18e1ad690af193fa55e159781c8063
tarun0582 <tarun.sharma@chicmic.co.in>
2023-02-28 18:03:48 +0530

    28Feb

--- src/app/components/players-stats/players-stats.component.ts
+++ src/app/components/players-stats/players-stats.component.ts
@@ -1,26 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlayersService } from 'src/app/services/players.service';
-import { players } from 'src/player';
 @Component({
   selector: 'app-players-stats',
   templateUrl: './players-stats.component.html',
   styleUrls: ['./players-stats.component.scss']
 })
 export class PlayersStatsComponent  {
-  players:any=players
-  newObj:any;
-  constructor(){
-    console.log(this.players,"gv")
-    const { "0": removeKey, ...newObj } = this.players;
-    console.log(newObj,"afa")
+  player:any
+  player1:any
+  playerData:any
+  constructor(private route: ActivatedRoute,private data: PlayersService) { 
+   
+ 
+  }
+  ngOnInit() {
+    this.data.getPlayer().subscribe((res: any) => {
+      this.route.params.subscribe(s =>{
+        console.log(s);
+        console.log(s);
+        this.player1=s;
+      })
+   
+      this.playerData = Object.keys(res).map((key) => { return res[key] });
+      console.log(this.playerData,"gdfthgv")
+      const routeParams = this.route.snapshot.paramMap;
+      const productIdFromRoute = Number(routeParams.get('player.id'));
+      console.log(productIdFromRoute)
+      console.log(this.playerData,"abc")
+      this.player = this.playerData.find((player:any) => player.id == productIdFromRoute);
+      console.log(this.player,"abvgf")
+    })
+   
   
-    // delete this.players["0"];
-    // console.log(this.players,"gv")
-    
+  
+  }
   }
 
-}
+
 
 
 
